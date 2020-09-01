c5ac0e77ec0bc12db135f61143e1a489f2252495
mayallzObject <mayallzobject@gmail.com>
2020-09-01 18:57:38 +0200

    rules-card

--- src/components/TableCards/RulesCard.tsx
+++ src/components/TableCards/RulesCard.tsx
@@ -1,63 +1,155 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { useDispatch } from "react-redux";
+// Mui components
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import {
+  Button,
+  Slide,
+  Fade,
+  Snackbar,
+  Tooltip,
+  CardActions,
+  withStyles,
+  Paper,
+} from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FaceIcon from "@material-ui/icons/Face";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../store/user/selectors";
 
-import { Button } from "@material-ui/core";
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {},
+    media: {
+      paddingTop: "56.25%", // 16:9
+      minHeight: 400,
+      minWidth: 380,
+      alignItems: "center",
+      borderRadius: 10,
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  })
+);
 
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: "#f5f5f9",
+    color: "rgba(0, 0, 0, 0.87)",
+    minWidth: 400,
+    fontSize: theme.typography.pxToRem(14),
+    border: "1px solid #dadde9",
+    textColor: "#000000",
+  },
+}))(Tooltip);
 
+function SlideTransition(props: any) {
+  return <Slide {...props} direction="up" />;
+}
 
+export default function PremierFounder(props: any) {
+  const classes = useStyles();
+  const user = useSelector(selectUser);
+  const [state, setState] = React.useState({
+    open: false,
+    Transition: Fade,
+  });
 
-export default function AddPizzaForm() {
-    const [name, set_name] = useState("");
-    const [description, set_description] = useState("");
+  const handleClick = (Transition: any) => () => {
+    setState({
+      open: true,
+      Transition,
+    });
+  };
 
-    const dispatch = useDispatch();
+  const handleClose = () => {
+    setState({
+      ...state,
+      open: false,
+    });
+  };
 
-    const submit = (event: any): void => {
-        // to make sure that the form does not redirect (which is normal browser behavior)
-        event.preventDefault();
+  return (
+    <Card elevation={23} className={classes.root}>
+      <Paper>
+        <CardHeader
+          title={user.name}
+          subheader="Codaisseur Academy Graduate"
+          action={
+            <Tooltip
+              color="primary"
+              TransitionComponent={Fade}
+              TransitionProps={{ timeout: 600 }}
+              title={<h2>{`Go to ${user.name}'s GitHub `}</h2>}
+            >
+              <Button variant="contained" color="primary" aria-label="settings">
+                <GitHubIcon fontSize="small" />
+              </Button>
+            </Tooltip>
+          }
+        />
 
-        console.log("new pizza:", name, description);
+        <Typography paragraph>
+          <Snackbar
+            open={state.open}
+            onClose={handleClose}
+            TransitionComponent={state.Transition}
+            message={<h1>{user.score}</h1>}
+            key={state.Transition.name}
+          />
+        </Typography>
 
-        const action = {
-            type: "ADD_PIZZA",
-            payload: {
-                id: Math.random(),
-                name: name,
-                description: description,
-            },
-        };
-        dispatch(action);
-    };
-
-    return (
-        <form onSubmit={submit}>
-            <h2>Add a new pizza</h2>
-            <p>
-                <label>
-                    Name:{" "}
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => set_name(e.target.value)}
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    Description:{" "}
-                    <input
-                        type="text"
-                        value={description}
-                        onChange={(e) => set_description(e.target.value)}
-                    />
-                </label>
-            </p>
-            <p>
-                <Button>
-                    Add this pizza!
-        </Button>
-            </p>
-        </form>
-    );
+        <CardMedia
+          className={classes.media}
+          //@ts-ignore
+          image={user.userImg}
+          title={props.name}
+        />
+        <CardContent>
+          <Typography paragraph>About me: {props.aboutMe}</Typography>
+        </CardContent>
+        <CardActions>
+          {/* 
+    //@ts-ignore           */}
+          <Button
+            elevation={23}
+            endIcon={<FaceIcon />}
+            variant="contained"
+            color="primary"
+            onClick={handleClick(SlideTransition)}
+          >
+            About
+          </Button>
+          <HtmlTooltip
+            title={
+              <React.Fragment>
+                <Typography color="inherit">Contact</Typography>
+                <p> email: {props.email}</p>
+                <p> LinkedIn: {props.linkedIn} </p>
+              </React.Fragment>
+            }
+          >
+            <Button>Contact</Button>
+          </HtmlTooltip>
+        </CardActions>
+      </Paper>
+    </Card>
+  );
 }
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,50 +1,48 @@
-//HomePage/ react / redux imports 
-import React from 'react'
+//HomePage/ react / redux imports
+import React from "react";
 
 //Components
 import QuestionCard from "../../components/Questions/Questions";
 
-
 //Mui components
-import { useStyles, } from "./mui"
+import { useStyles } from "./mui";
 
-import Grid from '@material-ui/core/Grid'
-import { Paper, Box } from '@material-ui/core';
-
+import Grid from "@material-ui/core/Grid";
+import { Paper, Box } from "@material-ui/core";
+import PremierFounder from "../../components/TableCards/RulesCard";
+import ScoreboardPage from "../../components/Scoreboard";
 
 type Props = {
-    darkMode: boolean
-    set_darkMode: (value: boolean) => void
-}
+  darkMode: boolean;
+  set_darkMode: (value: boolean) => void;
+};
 
 export default function Home(props: Props) {
-    const classes = useStyles()
+  const classes = useStyles();
 
-
-    return (
-        <Grid className={classes.paper} container spacing={2}>
-
-
-            <Grid item xs={12} sm={4}>
-                <Box mt={10}>
-                    <Paper style={{ marginTop: 20, minHeight: 500, borderRadius: 150, backgroundImage: "url(https://image.freepik.com/free-photo/microprocessor-chipset-central-processor-unit-illumination-circuit_34645-889.jpg)" }}>
-                    </Paper>
-                </Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-                <Box mt={10}>
-                    <Paper style={{ marginTop: 20, minHeight: 500, borderRadius: 150, backgroundImage: "url(https://cdn.wccftech.com/wp-content/uploads/2016/09/spacee-740x463.jpg)" }}>
-                        <QuestionCard />
-                    </Paper>
-                </Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-                <Box mt={10}>
-                    <Paper style={{ marginTop: 20, minHeight: 500, borderRadius: 150, backgroundImage: "url(https://image.freepik.com/free-photo/microprocessor-chipset-central-processor-unit-illumination-circuit_34645-889.jpg)" }}>
-
-                    </Paper>
-                </Box>
-            </Grid>
-        </Grid>
-    )
+  return (
+    <Grid className={classes.paper} container spacing={2}>
+      <Grid item xs={12} sm={4}>
+        <Box mt={10}>
+          <Paper>
+            <ScoreboardPage />
+          </Paper>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Box mt={10}>
+          <Paper>
+            <QuestionCard />
+          </Paper>
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Box mt={10}>
+          <Paper>
+            <PremierFounder />
+          </Paper>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
